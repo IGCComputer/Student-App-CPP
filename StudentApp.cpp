a2b4741e3ad7a6d405608ee00bcf5bcc9b0cdc72
IGCComputer <igcdemo640@gmail.com>
2023-03-03 21:17:38 +0530

    [ADDED]:[Tahir] Student Info By using CPP Code

---
+++ StudentApp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int id;
+	char name[10];
+	float fees;
+	
+	cout<<"\nEnter the ID:";
+	cin>>id;
+	cout<<"\nEnter the Name:";
+	cin>>name;
+	cout<<"\nEnter the Fees:";
+	cin>>fees;
+	
+	cout<<"\nID:"<<id;
+	cout<<"\nName:"<<name;
+	cout<<"\nFees:"<<fees;
+	
+	return 0;
+}
